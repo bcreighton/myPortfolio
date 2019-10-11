1a5c03ebb30d52fc44be0db88ced098025271177
Bryson Creighton <brysoncreighton@gmail.com>
2019-10-10 21:12:52 -0600

    remote dev... continue function development. added data set

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+/* Project Data Set */
+
+const projects = [
+    {
+        id: CUID,
+        title: 'Sports Quiz',
+        Description: 'The Sports Trivia Game is an application that tests your sports knowledge with random sports questions from different types of sports. Are you one of the GOATs or is your sports knowledge mediocre?',
+        tech: 'HTML, CSS, JQuery',
+        link: 'https://bcreighton.github.io/sportsTriviaGame/',
+        repo: 'https://github.com/bcreighton/sportsTriviaGame'
+    },
+    {
+        id: CUID,
+        title: 'Project',
+        Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        tech: 'HTML, CSS, JQuery',
+        link: 'http',
+        repo: 'http'
+    },
+    {
+        id: CUID,
+        title: 'Project',
+        Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        tech: 'HTML, CSS, JQuery',
+        link: 'http',
+        repo: 'http'
+    },
+    {
+        id: CUID,
+        title: 'Project',
+        Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        tech: 'HTML, CSS, JQuery',
+        link: 'http',
+        repo: 'http'
+    }
+]
+
+
 /* Functionality
 
 Nav bar will condense for mobile and will handle click
