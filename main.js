898b11ea359db006c39cf530941fffc21ad19aff
Bryson Creighton <brysoncreighton@gmail.com>
2019-10-12 01:19:28 -0600

    added hover functionality with proper color switching for each project

--- main.js
+++ main.js
@@ -2,36 +2,64 @@
 
 const projects = [
     {
-        id: CUID,
+        id: 'CUID',
         title: 'Sports Quiz',
-        Description: 'The Sports Trivia Game is an application that tests your sports knowledge with random sports questions from different types of sports. Are you one of the GOATs or is your sports knowledge mediocre?',
+        image: 'images\sportsQuiz.jpg',
+        description: 'The Sports Trivia Game is an application that tests your sports knowledge with random sports questions from different types of sports. Are you one of the GOATs or is your sports knowledge mediocre?',
         tech: 'HTML, CSS, JQuery',
         link: 'https://bcreighton.github.io/sportsTriviaGame/',
-        repo: 'https://github.com/bcreighton/sportsTriviaGame'
+        repo: 'https://github.com/bcreighton/sportsTriviaGame',
+        hoverColor: 'red'
     },
     {
-        id: CUID,
-        title: 'Project',
-        Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        id: 'CUID',
+        title: 'Project 2',
+        image: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
-        repo: 'http'
+        repo: 'http',
+        hoverColor: 'yellow'
     },
     {
-        id: CUID,
-        title: 'Project',
-        Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        id: 'CUID',
+        title: 'Project 3',
+        image: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
-        repo: 'http'
+        repo: 'http',
+        hoverColor: 'green'
     },
     {
-        id: CUID,
-        title: 'Project',
-        Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        id: 'CUID',
+        title: 'Project 4',
+        image: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
-        repo: 'http'
+        repo: 'http',
+        hoverColor: 'purple'
+    },
+    {
+        id: 'CUID',
+        title: 'Project 5',
+        image: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        tech: 'HTML, CSS, JQuery',
+        link: 'http',
+        repo: 'http',
+        hoverColor: 'pink'
+    },
+    {
+        id: 'CUID',
+        title: 'Project 6',
+        image: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        tech: 'HTML, CSS, JQuery',
+        link: 'http',
+        repo: 'http',
+        hoverColor: 'blue'
     }
 ]
 
@@ -52,8 +80,40 @@ function handleNavClicks() {
     console.log('listening for nav clicks');
 }
 
+function generateProjects(projects) {
+    for (let i = 0; i < projects.length; i++) {
+        $('.projectContainer').append(`
+            <div class="project">
+            <div class="content">
+                <h3 class = 'projectTitle'>${projects[i]['title']}</h3>
+                <p class = 'projectDescription'>${projects[i]['description']}</p>
+            </div>
+            `);
+    }
+}
+
 function handleProjectHover() {
     console.log('looking for project hovers');
+    $('.projectContainer').delegate('.project', 'mouseover mouseout', function(e) {
+
+        let projTitle;
+        let bgColor;
+
+        if(e.type == 'mouseover') {
+            projTitle = $(this).find('.projectTitle').text();
+            $(this).find('.content').css('display', 'block');
+            bgColor = $.grep(projects, function(proj){
+                    return proj.title === projTitle;
+                    })
+                    [0]['hoverColor'];
+            $(this).css('background-color', bgColor);
+            $('body').css('background-color', bgColor);
+        } else if(e.type == 'mouseout') {
+            $(this).find('.content').css('display', 'none');
+            $(this).css('background-color', 'rgba(215, 201, 170, 1)');
+            $('body').css('background-color', 'rgba(247, 247, 247, 1)');
+        }
+    });
 }
 
 function handleSlideshowClicks() {
@@ -65,6 +125,7 @@ function valueSlideshow() {
 }
 
 function portfolioListener() {
+    generateProjects(projects);
     handleNavClicks();
     handleProjectHover();
     handleSlideshowClicks();
