4ccf389cc8def5a9228ecea581f4552863c29174
Bryson Creighton <brysoncreighton@gmail.com>
2019-10-13 16:32:54 -0600

    ironed out hover functionality and added readMore option with links to the live site and repo

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const projects = [
         id: 'CUID',
         title: 'Sports Quiz',
         image: 'images/sportsQuiz.jpg',
-        description: 'The Sports Trivia Game is an application that tests your sports knowledge with random sports questions from different types of sports. Are you one of the GOATs or is your sports knowledge mediocre?',
+        description: 'The Sports Trivia Game is an application that tests your sports knowledge<span class="dots">...</span><button class="readMore">Read More</button><span class="moreText"> with random sports questions from different types of sports. Are you one of the GOATs or is your sports knowledge mediocre?</span><button class="readLess">Read Less</button>',
         tech: 'HTML, CSS, JQuery',
         link: 'https://bcreighton.github.io/sportsTriviaGame/',
         repo: 'https://github.com/bcreighton/sportsTriviaGame',
@@ -15,7 +15,7 @@ const projects = [
         id: 'CUID',
         title: 'Project 2',
         image: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae<span class="dots">...</span><button class="readMore">Read More</button><span class="moreText"> iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.</span><button class="readLess">Read Less</button>',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
         repo: 'http',
@@ -25,7 +25,7 @@ const projects = [
         id: 'CUID',
         title: 'Project 3',
         image: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae<span class="dots">...</span><button class="readMore">Read More</button><span class="moreText"> iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.</span><button class="readLess">Read Less</button>',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
         repo: 'http',
@@ -35,7 +35,7 @@ const projects = [
         id: 'CUID',
         title: 'Project 4',
         image: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae<span class="dots">...</span><button class="readMore">Read More</button><span class="moreText"> iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.</span><button class="readLess">Read Less</button>',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
         repo: 'http',
@@ -45,7 +45,7 @@ const projects = [
         id: 'CUID',
         title: 'Project 5',
         image: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae<span class="dots">...</span><button class="readMore">Read More</button><span class="moreText"> iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.</span><button class="readLess">Read Less</button>',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
         repo: 'http',
@@ -55,7 +55,7 @@ const projects = [
         id: 'CUID',
         title: 'Project 6',
         image: '',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse vitae<span class="dots">...</span><button class="readMore">Read More</button><span class="moreText"> iaculis erat. Phasellus id ante sem. Sed dui metus, vulputate at tempor a, luctus in elit. Sed pulvinar dolor a nulla sodales, eu malesuada nisi malesuada.</span><button class="readLess">Read Less</button>',
         tech: 'HTML, CSS, JQuery',
         link: 'http',
         repo: 'http',
@@ -88,6 +88,11 @@ function generateProjects(projects) {
                 <div class='content'>
                     <h3 class = 'projectTitle'>${projects[i]['title']}</h3>
                     <p class = 'projectDescription'>${projects[i]['description']}</p>
+                    <p class = 'techUsed'>${projects[i]['tech']}</p>
+                    <div class = 'projectLinks'>
+                        <a class = 'live' href='${projects[i]["link"]}'>Live</a>
+                        <a class = 'repo' href='${projects[i]["repo"]}'>Repo</a>
+                    </div>
                 </div>
             </div>
             `);
@@ -116,12 +121,14 @@ function handleProjectHover() {
                 [0]['image'];
             $(this).css('background-image',`url('${bgImage}')`);
             $(this).find('.projectTint').css('background-color', bgColor);
+            $('.project').css('background-color', bgColor);
             $('body').css('background-color', bgColor);
         } else if(e.type == 'mouseout') {
             $(this).css('background-image','none');
             $(this).find('.projectTint').css('display', 'none');
             $(this).find('.content').css('display', 'none');
             $(this).css('background-color', 'rgba(215, 201, 170, 1)');
+            $('.project').css('background-color', 'rgba(226, 226, 226, 1)');
             $('body').css('background-color', 'rgba(247, 247, 247, 1)');
         }
     });
@@ -135,12 +142,73 @@ function valueSlideshow() {
     console.log('slideshow is running');
 }
 
+function handleReadMoreClicks() {
+    const dots = document.getElementsByClassName('dots');
+    const moreText = document.getElementsByClassName('moreText');
+    const readMore = document.getElementsByClassName('readMore');
+
+    $('.projectContainer').on('click', '.readMore', function() {
+        if ($('.dots').css('display') === 'none') {
+            $('.dots').css('display', 'inline');
+            $('.readMore').css('display','inline');
+            $('.moreText').css('display', 'none');
+            $('.techUsed').css('display', 'block');
+            $('.projectLinks').css('display', 'block');
+        } else {
+            $('.dots').css('display', 'none');
+            $('.readMore').css('display','none');
+            $('.moreText').css('display', 'inline');
+            $('.readLess').css('display', 'inline');
+            $('.techUsed').css('display', 'none');
+            $('.projectLinks').css('display', 'none');
+        }
+    });
+
+    $('.projectContainer').delegate('.project', 'mouseleave', function() {
+        $('.dots').css('display', 'inline');
+        $('.readMore').css('display','inline');
+        $('.moreText').css('display', 'none');
+        $('.readLess').css('display', 'none');
+        $('.techUsed').css('display', 'block');
+        $('.projectLinks').css('display', 'block');
+    });
+}
+
+function handleReadLessClicks() {
+    const dots = document.getElementsByClassName('dots');
+    const moreText = document.getElementsByClassName('moreText');
+    const readMore = document.getElementsByClassName('readMore');
+
+    $('.projectContainer').on('click', '.readLess', function() {
+
+        if ($('.dots').css('display') === 'none') {
+            $('.dots').css('display', 'inline');
+            $('.readMore').css('display','inline');
+            $('.moreText').css('display', 'none');
+            $('.readLess').css('display', 'none');
+            $('.techUsed').css('display', 'block');
+            $('.projectLinks').css('display', 'block');
+        } else {
+            $('.dots').css('display', 'none');
+            $('.readMore').css('display','none');
+            $('.moreText').css('display', 'inline');
+            $('.readLess').css('display', 'inline');
+            $('.techUsed').css('display', 'none');
+            $('.projectLinks').css('display', 'none');
+        }
+    });
+}
+
+
+
 function portfolioListener() {
     generateProjects(projects);
     handleNavClicks();
     handleProjectHover();
     handleSlideshowClicks();
-
+    handleReadMoreClicks();
+    handleReadLessClicks();
+    
     /*Non listener*/
     valueSlideshow();
 }
